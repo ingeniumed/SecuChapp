d32d51534d11f195644be5bc452fd411beb8e8b2
Gopal Krishnan <gk.krishnan92@gmail.com>
2012-11-14 18:02:24 -0500

    Created the main screen for showing the conversations. Left: 1) Creating a list view for the conversations 	  2) move log out to overflow menu along with settings 	  3) fix animations

--- SecuChapp/src/com/application/secuchapp/SecureChat.java
+++ SecuChapp/src/com/application/secuchapp/SecureChat.java
@@ -1,20 +1,28 @@
 package com.application.secuchapp;
 
 import android.os.Bundle;
+import android.content.Intent;
 import android.app.Activity;
 import android.view.Menu;
-//IT WORKS
+import android.view.View;
+import android.widget.Button;
+import android.animation.LayoutTransition;
+
 public class SecureChat extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secure_chat);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_secure_chat, menu);
-        return true;
+    Button log_in = (Button) findViewById (R.id.log_in);
+    log_in.setOnClickListener(new View.OnClickListener(){
+    	public void onClick(View view) {
+    		Intent intent_1 = new Intent(view.getContext(),MainScreen.class);
+       		overridePendingTransition(R.anim.rotate_out,R.anim.rotate_in);
+    		startActivityForResult(intent_1,0);
+    	}
+    });
+    
     }
 }
