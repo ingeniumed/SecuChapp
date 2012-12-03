10775a7e4a53af0237d0eb144e1132e847649852
Gopal Krishnan <gk.krishnan92@gmail.com>
2012-12-03 16:35:54 -0500

    port change

--- SecuChapp/src/com/application/secuchapp/TCPClient.java
+++ SecuChapp/src/com/application/secuchapp/TCPClient.java
@@ -15,7 +15,7 @@ import java.net.Socket;
 public class TCPClient {
  
     private String serverMessage;
-    public static final String SERVERIP = "192.168.2.13"; //your computer IP address
+    public static final String SERVERIP = "172.17.146.62"; //your computer IP address
     public static final int SERVERPORT = 4454;
     private OnMessageReceived mMessageListener = null;
     private boolean mRun = false;
